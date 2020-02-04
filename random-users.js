1c756627008dd713a8d72394160aee65194044d9
rcntype@gmail.com <rcntype@gmail.com>
2020-02-04 17:02:44 -0500

    refactor

--- random-users.js
+++ random-users.js
@@ -3,51 +3,37 @@ const randomPromise2 = fetch('https://acme-users-api-rev.herokuapp.com/api/users
 const randomPromise3 = fetch('https://acme-users-api-rev.herokuapp.com/api/users/random').then(response => response.json())
 
 const bigContainer = document.querySelector('#big-container')
-const pages = document.querySelector('#pages')
 
-window.addEventListener('hashchange', () => {
-    const id = window.location.hash.slice(1)
-    fetch(`https://acme-users-api-rev.herokuapp.com/api/users/random/${id}`)
-        .then(response => response.json())
-        .then( result => renderPages(result))
-})
+const smallContainer = document.querySelectorAll('div .small-container')
 
-const id = window.location.hash.slice(1)
-if (id) {
-    let element = document.getElementById('small-container');
-    element.classList.toggle('hidden');
-}
+console.log(smallContainer)
 
 
 const render = (data) => {
-    const html = data.map(person => {
-        const name = person.fullName
-        const email = person.email
-        const pic = person.avatar
-        
-        return `
-        <div id='small-container'>
-            <div class='person-name'>${name}</div>
-            <div class='person-email'>${email}</div>
-            <img src='${pic}'/>
+    const html = data.map((person, idx) => {
+        const count = idx+1
+        return `    
+        <div class='links'><a href="#${person.id}">${count}</a></div>    
+        <div class='small-container'>
+            <div class='person-name'>${person.fullName}</div>
+            <div class='person-email'>${person.email}</div>
+            <img src='${person.avatar}'/>
         </div>`
     }).join('')
     bigContainer.innerHTML = html
 }
 
-const renderPages = (data) => {
-    const html = data.map((x, idx) => {
-        const count = idx + 1
-        return `<li class="links"><a href="#${idx}">${count}</a></li>`
-    }).join('')
-    pages.innerHTML = html
-}
 
 
-Promise.all([randomPromise1, randomPromise2, randomPromise3])
+
+
+const p = Promise.all([randomPromise1, randomPromise2, randomPromise3])
     .then(data => {
         render(data)
-        renderPages(data)
     })
 
    
+window.addEventListener('hashchange', () => {
+    const id = window.location.hash.slice(1)
+    
+})
