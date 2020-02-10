7e87f73869ecb5acbd16ac2e99e678ead9c2fd98
Peet Klecha <p.a.klecha@gmail.com>
2020-02-10 13:07:59 -0500

    comments

--- random-users.js
+++ random-users.js
@@ -1,39 +1,43 @@
-const randomPromise1 = fetch('https://acme-users-api-rev.herokuapp.com/api/users/random').then(response => response.json())
-const randomPromise2 = fetch('https://acme-users-api-rev.herokuapp.com/api/users/random').then(response => response.json())
-const randomPromise3 = fetch('https://acme-users-api-rev.herokuapp.com/api/users/random').then(response => response.json())
+//[pk] could make this terser by putting the API URL in a variable, and also by doing the .JSONing inside the promise.all
+const randomPromise1 = fetch(
+	"https://acme-users-api-rev.herokuapp.com/api/users/random"
+).then(response => response.json())
+const randomPromise2 = fetch(
+	"https://acme-users-api-rev.herokuapp.com/api/users/random"
+).then(response => response.json())
+const randomPromise3 = fetch(
+	"https://acme-users-api-rev.herokuapp.com/api/users/random"
+).then(response => response.json())
 
-const bigContainer = document.querySelector('#big-container')
+const bigContainer = document.querySelector("#big-container")
 
-const smallContainer = document.querySelectorAll('div .small-container')
+const smallContainer = document.querySelectorAll("div .small-container")
 
 console.log(smallContainer)
 
-
-const render = (data) => {
-    const html = data.map((person, idx) => {
-        const count = idx+1
-        return `    
-        <div class='links'><a href="#${person.id}">${count}</a></div>    
+const render = data => {
+	const html = data
+		.map((person, idx) => {
+			const count = idx + 1
+			return `
+        <div class='links'><a href="#${person.id}">${count}</a></div>
         <div class='small-container'>
             <div class='person-name'>${person.fullName}</div>
             <div class='person-email'>${person.email}</div>
             <img src='${person.avatar}'/>
         </div>`
-    }).join('')
-    bigContainer.innerHTML = html
+		})
+		.join("")
+	bigContainer.innerHTML = html
 }
 
+const p = Promise.all([randomPromise1, randomPromise2, randomPromise3]).then(
+	data => {
+		render(data)
+	}
+)
 
-
-
-
-const p = Promise.all([randomPromise1, randomPromise2, randomPromise3])
-    .then(data => {
-        render(data)
-    })
-
-   
-window.addEventListener('hashchange', () => {
-    const id = window.location.hash.slice(1)
-    
+window.addEventListener("hashchange", () => {
+	const id = window.location.hash.slice(1)
+	//[pk] we talked abt this in office hours...
 })
